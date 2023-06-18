eaebe5a8004e0923862ee5115c57b575b6719c92
Manish Kumar <manish.login01@gmail.com>
2023-06-18 11:27:17 +0530

    Pushing the second version with few changes. Added toggle option for the button. And started designing budget list layout

--- src/app.js
+++ src/app.js
@@ -6,11 +6,53 @@ selectElement.addEventListener("change", () => {
 });
 
 // to display month and year
-const currentDate = new Date();
-const month = currentDate.toLocaleString("default", { month: "long" });
-const year = currentDate.getFullYear();
+function monthYear() {
+  const currentDate = new Date();
+  const month = currentDate.toLocaleString("default", { month: "long" });
+  const year = currentDate.getFullYear();
 
-const currentMonthYear = `${month} ${year}`;
+  const currentMonthYear = `${month} ${year}`;
+  return currentMonthYear;
+}
 
 const displayMonthYear = document.querySelector(".month-year");
-displayMonthYear.innerHTML = currentMonthYear;
+displayMonthYear.innerHTML = monthYear();
+
+// function for date format
+function fullDate() {
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+
+  const formattedDate = `${day}/${month}/${year}`;
+  return formattedDate;
+}
+
+// for button-toggle
+const tickIcon = document.querySelector(".tick.add-button.fas.fa-check-circle");
+
+function toggleTickAndText() {
+  if (window.innerWidth < 850) {
+    tickIcon.classList.add("add-button");
+    tickIcon.classList.remove("tick");
+    tickIcon.classList.remove("fas");
+    tickIcon.classList.remove("fa-check-circle");
+    tickIcon.textContent = "ADD";
+  } else {
+    tickIcon.classList.remove("add-button");
+    tickIcon.classList.add("tick");
+    tickIcon.classList.add("fas");
+    tickIcon.classList.add("fa-check-circle");
+    tickIcon.textContent = "";
+  }
+}
+
+toggleTickAndText();
+
+window.addEventListener("resize", toggleTickAndText);
+
+// tick-icon event-listener
+tickIcon.addEventListener("click", () => {
+  showExpenses();
+});
